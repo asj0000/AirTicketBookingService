036c94f38bde034f18ff18fe0c8713ac989870a3
Ashish_Jain <121597042+asj0000@users.noreply.github.com>
2025-09-17 13:48:35 +0530

    added rabbit mq setup

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,73 +1,94 @@
 const { BookingService }= require( '../services/index');
 const bookingServiceObj = new BookingService();
 const { StatusCodes } = require('http-status-codes')
+const { createChannel, publishMessage } = require('../utils/messageQueue')
+const { REMINDER_BINDING_KEY}  = require('../config/serverConfig')
 
-const create = async ( req , res )=>{
-    try {
-        const response = await bookingServiceObj.createBooking( req.body );
-        return res.status( StatusCodes.OK).json({
-            data: response,
-            success: true,
-            message: "Successfuly created a booking",
-            err: {}
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(error.statusCode).json({
-            data:{},
-            success: false,
-            message: error.message,
-            err: error.explanation
-        })
+class BookingController{
+    
+    async sendToQueue(req , res ){
+        try {
+            const channel = await createChannel();
+            console.log("Channel created in controller");
+            const data = { message: 'Success'};
+            publishMessage(channel , REMINDER_BINDING_KEY , JSON.stringify( data ));
+            return res.status(StatusCodes.OK).json({
+                message: "Successfully published the event",
+                err: {}
+            });
+        } catch (error) {
+            return res.status(500).json({
+                data: {},
+                success: false,
+                message: error.message,
+                err: error.explanation
+            });
+        }
+    }
+    async create(req, res) {
+        try {
+            const response = await bookingServiceObj.createBooking(req.body);
+            return res.status(StatusCodes.OK).json({
+                data: response,
+                success: true,
+                message: "Successfully created a booking",
+                err: {}
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(error.statusCode).json({
+                data: {},
+                success: false,
+                message: error.message,
+                err: error.explanation
+            });
+        }
+    }
+    async get( req , res ){
+        try {
+            const bookingId = req.params.id ;
+            const response = await bookingServiceObj.getBooking( bookingId);
+            return res.status( StatusCodes.OK).json({
+                data: response,
+                success: true,
+                message: "Successfuly fetched a booking",
+                err: {}
+            }) 
+        } catch (error) {
+            console.log("error caught in controller ",error);
+            return res.status(error.statusCode).json({
+                data:{},
+                success: false,
+                message: error.message,
+                err: error.explanation
+            })
+            
+        }
+    }
+
+    async update( req , res){
+        try {
+            const response = await bookingServiceObj.updateBooking( req.params.id , req.body);
+            return res.status( StatusCodes.OK).json({
+                data: response,
+                success: true,
+                message: "Successfuly updated a booking",
+                err: {}
+            }) 
+        } catch (error) {
+            console.log("error caught in controller ",error);
+            return res.status(error.statusCode).json({
+                data:{},
+                success: false,
+                message: error.message,
+                err: error.explanation
+            })
+        }
     }
 
 }
 
-const get = async( req , res )=>{
-    try {
-        const bookingId = req.params.id ;
-        const response = await bookingServiceObj.getBooking( bookingId);
-        return res.status( StatusCodes.OK).json({
-            data: response,
-            success: true,
-            message: "Successfuly fetched a booking",
-            err: {}
-        }) 
-    } catch (error) {
-        console.log("error caught in controller ",error);
-        return res.status(error.statusCode).json({
-            data:{},
-            success: false,
-            message: error.message,
-            err: error.explanation
-        })
-        
-    }
-}
 
-const update = async( req, res )=>{
-    try {
-        const response = await bookingServiceObj.updateBooking( req.params.id , req.body);
-        return res.status( StatusCodes.OK).json({
-            data: response,
-            success: true,
-            message: "Successfuly updated a booking",
-            err: {}
-        }) 
-        
-    } catch (error) {
-        console.log("error caught in controller ",error);
-        return res.status(error.statusCode).json({
-            data:{},
-            success: false,
-            message: error.message,
-            err: error.explanation
-        })
-    }
-}
 
-module.exports = {
-    create,
-    get,
-    update
-}
+
+module.exports  = BookingController;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { BookingController } = require('../../controllers/index');
+
+const { BookingController} = require('../../controllers')
+const { createChannel } = require('../../utils/messageQueue')
 const router = express.Router();
 
-router.post('/bookings' , BookingController.create);
-router.get('/bookings/:id', BookingController.get );
-router.patch('/bookings/:id', BookingController.update);
+
+const bookingControllerObj = new BookingController();
+
+router.post('/bookings' , bookingControllerObj.create);
+router.post('/publish' , bookingControllerObj.sendToQueue);
+router.get('/bookings/:id', bookingControllerObj.get );
+router.patch('/bookings/:id', bookingControllerObj.update);
 
 
 module.exports = router;
